49700014ce3f63023ab21e4d4a6bd4b1646776e5
Korey Cadiz <korey.cadiz@duke.edu>
2024-02-05 16:45:39 -0500

    update

--- TPC.R
+++ TPC.R
@@ -3,3 +3,48 @@ library(ggplot2)
 library(nls.multstart)
 
 
+#| label: plot-xtemp-yfinal
+
+ggplot(TPC, aes(x = temp, y = final, color = rep, shape = bact)) +
+  geom_point () +
+  geom_smooth(se = FALSE) +
+  labs (
+    x = "Temperature",
+    y = "Count",
+    title = "Thermal Performance Curve",
+    color = "Replicate",
+    shape = "Bacteria"
+  )
+
+
+#| label: plot-xtemp-yfinal
+
+ggplot(TPC, aes(x = temp, y = final, color = rep)) +
+  geom_point () +
+  geom_smooth(se = FALSE) +
+  facet_wrap(~bact) +
+  labs (
+    x = "Temperature",
+    y = "Count",
+    title = "Thermal Performance Curve",
+    color = "Replicate"
+  )
+
+
+#| label: plot-xtemp-yfinalavg
+
+TPC <- TPC |>
+  group_by(bact, temp) |>
+  mutate(finalavg = mean(final, na.rm = TRUE)) 
+
+ggplot(TPC, aes(x = temp, y = finalavg, shape = bact)) +
+  geom_point() +
+  labs (
+    x = "Temperature",
+    y = "Average Count",
+    title = "Thermal Performance Curve",
+    shape = "Bacteria"
+  )
+
+
+
